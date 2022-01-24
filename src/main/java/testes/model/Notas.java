751d16e29e76b2af6e05249b9aa3820f4d5bbdbf
Fernando <joaofernandoscsc@gmail.com>
2022-01-24 17:33:02 -0300

    upload

---
+++ src/main/java/testes/model/Notas.java
@@ -0,0 +1,14 @@
+package testes.model;
+
+public class Notas {
+
+	public Double[] notas;notas[]=
+	{3, 2,1};
+
+	public double todasNotas (){
+			for(int i; i < notas.length ; i++ ){
+			return 
+		}
+			}
+
+}
