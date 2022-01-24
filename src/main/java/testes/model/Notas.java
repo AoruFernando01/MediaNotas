9654ca240891e4a88b3cb4f43d49b6ef7c4a3be4
Fernando <joaofernandoscsc@gmail.com>
2022-01-24 18:40:17 -0200

    alo

--- src/main/java/testes/model/Notas.java
+++ src/main/java/testes/model/Notas.java
@@ -2,13 +2,9 @@ package testes.model;
 
 public class Notas {
 
-	public Double[] notas;notas[]=
-	{3, 2,1};
+	public Double[] notas;
+	notas[] =	 [3, 2,1];
 
-	public double todasNotas (){
-			for(int i; i < notas.length ; i++ ){
-			return 
-		}
-			}
+			
 
 }
